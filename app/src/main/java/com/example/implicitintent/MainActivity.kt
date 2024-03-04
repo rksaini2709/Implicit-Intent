8e371d0b22436dc38f43088ef01fa97809cbacd7
rksaini2709 <ritiksaini8060@gmail.com>
2024-03-04 14:13:54 +0530

    Implicit Intent - Web Page, maps

--- app/src/main/java/com/example/implicitintent/MainActivity.kt
+++ app/src/main/java/com/example/implicitintent/MainActivity.kt
@@ -21,8 +21,44 @@ class MainActivity : AppCompatActivity() {
             // intent.data = Uri.parse("https::/www.google.com")
             intent.data = Uri.parse("https://sanatanilibrary.blogspot.com/")
             */
+
+
+            /*
+            // Map Location
             var intent = Intent(Intent.ACTION_VIEW,Uri.parse("geo: 30.1060,77.4675"))
             startActivity(Intent.createChooser(intent, "select your application"))
+             */
+
+
+            /*
+            // call a number
+            var intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: 7668664131"))
+            startActivity(intent)
+             */
+
+
+            /*
+            // send Email
+            var intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/rfc822"
+            intent.putExtra(Intent.EXTRA_SUBJECT,"Invition")
+            intent.putExtra(Intent.EXTRA_TEXT, "my birthday party on 27 Sep")
+            // intent.putExtra(Intent.EXTRA_EMAIL,arrayOf("itsrk2709@gmai.com"))
+            intent.putExtra(Intent.EXTRA_EMAIL,arrayOf("itsrk2709@gmai.com", "ritksaini8060@gamil.com")) // multiple
+            startActivity(intent)
+             */
+
+
+            /*
+            // send text by email, massage, whtsup, insta etc.
+            var intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain" // Set the MIME type to plain text
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Invitation")
+            intent.putExtra(Intent.EXTRA_TEXT, "My birthday party on 27 Sep")
+            startActivity(intent)
+             */
+
+
         }
     }
 }
